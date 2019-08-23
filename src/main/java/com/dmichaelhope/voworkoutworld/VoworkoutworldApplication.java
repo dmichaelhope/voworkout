3d66a3187872f05df38414f94463baba2c20e9d4
David Hope <dhope@ara.com>
2019-08-23 15:16:14 -0400

    from spring initializer

---
+++ src/main/java/com/dmichaelhope/voworkoutworld/VoworkoutworldApplication.java
@@ -0,0 +1,13 @@
+package com.dmichaelhope.voworkoutworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoworkoutworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoworkoutworldApplication.class, args);
+	}
+
+}
